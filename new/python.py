90fa989480213d324268a3346b9571bbed2549be
dhanabalopti <104548681+dhanabalopti@users.noreply.github.com>
2022-09-23 20:37:33 +0530

    Update python.py

--- new/python.py
+++ new/python.py
@@ -1,4 +1,5 @@
 import boto3
+pipeline = boto3.client('codepipeline')
 region = 'ap-south-1'
 instance = ['i-01121e78e6807681e']
 ec2 = boto3.client('ec2', region_name=region)
@@ -6,3 +7,8 @@ ec2 = boto3.client('ec2', region_name=region)
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instance)
     print('started your instances: ' + str(instance))
+    
+    response = pipeline.put_job_success_result(
+        jobId=event['CodePipeline.job']['id']
+    )
+    return response
