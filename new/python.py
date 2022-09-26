f77ed9832a4ae4ed1a36d5d7d9fe225abe2daa1d
dhanabalopti <104548681+dhanabalopti@users.noreply.github.com>
2022-09-26 14:13:47 +0530

    Update python.py

--- new/python.py
+++ new/python.py
@@ -1,8 +1,8 @@
 import boto3
 pipeline = boto3.client('codepipeline')
-region = 'ap-south-1'
+regions = 'ap-south-1'
 instance = ['i-01121e78e6807681e']
-ec2 = boto3.client('ec2', region_name=region)
+ec2 = boto3.client('ec2', region_name=regions)
 
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instance)
